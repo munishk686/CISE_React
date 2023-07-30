031a8bb4477050472cf72e5a1d8bb71d695a0478
munishk686 <munishk686@gmail.com>
2023-07-30 19:37:02 +1200

    react7

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,7 +6,7 @@ import App from './App';
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
-});
+  });
 
 describe('Addition', () => {
   it('knows that 2 and 2 make 4', () => {
